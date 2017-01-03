bc96d0098460eb862d1eb638345c0a4d31f43536
kormin <abaotom14@gmail.com>
2017-01-03 23:41:36 +0800

    add themes

---
+++ theme.bat
@@ -0,0 +1,2 @@
+rundll32.exe %SystemRoot%\system32\shell32.dll,Control_RunDLL %SystemRoot%\system32\desk.cpl desk,@Themes /Action:OpenTheme /file:"C:\Windows\Resources\Themes\aero.theme"
+rundll32.exe %SystemRoot%\system32\shell32.dll,Control_RunDLL %SystemRoot%\system32\desk.cpl desk,@Themes /Action:OpenTheme /file:"C:\Windows\Resources\Ease of Access Themes\hcblack.theme"
