9a0f392d58ca4832eacc71ab56a4451336bbb35c
kormin <abaotom14@gmail.com>
2017-01-10 00:21:45 +0800

    add user input

--- wifiToggle.bat
+++ wifiToggle.bat
@@ -1,6 +1,9 @@
-netsh wlan show networks | FIND "Wi-Fi" /I /C
-if %errorlevel% equ 1 (
-	netsh interface set interface name="Wi-Fi" admin=enable
-) else (
-	netsh interface set interface name="Wi-Fi" admin=disable
-)
+@echo off
+set /p wifi="Enter name of Wi-Fi connection: "
+netsh wlan show networks | FIND "%wifi%" /I /C
+rem if %errorlevel% equ 1 (
+rem 	netsh interface set interface name="Wi-Fi" admin=enable
+rem ) else (
+rem 	netsh interface set interface name="Wi-Fi" admin=disable
+rem )
+pause
