743a316f738a6541179ba3d205ae593028ad631d
kormin <abaotom14@gmail.com>
2017-01-10 00:53:42 +0800

    add user input

--- usbVirus.bat
+++ usbVirus.bat
@@ -1,1 +1,6 @@
-attrib -h -s -r -a /s /d Drive Name:*.*
+@echo off
+rem request for user input
+set /p fdname="Enter name of flash drive: "
+rem echo %fdname%
+attrib -h -s -r -a /s /d %fdname%:*.*
+pause
--- wifiToggle.bat
+++ wifiToggle.bat
@@ -1,12 +1,15 @@
 @echo off
 set file=wifiName
 if not exist %file%.txt (
+	rem request for user input
 	set /p wifi="Enter name of Wi-Fi connection: "
+	rem write to file
 	@echo %wifi% > %file%.txt
 	rem echo %file%.txt does not exist
 	pause
 	exit
 )
+rem read from file
 set /p wfval=<%file%.txt
 rem echo %wfval%
 netsh wlan show networks | FIND "%wfval%" /I /C
