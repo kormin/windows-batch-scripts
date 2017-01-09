7d9d323318a2d2c4f4d4a41ff75a09a9e2ac4454
kormin <abaotom14@gmail.com>
2017-01-10 00:37:25 +0800

    add file handling

--- wifiToggle.bat
+++ wifiToggle.bat
@@ -1,6 +1,15 @@
 @echo off
-set /p wifi="Enter name of Wi-Fi connection: "
-netsh wlan show networks | FIND "%wifi%" /I /C
+set file=wifiName
+if not exist %file%.txt (
+	set /p wifi="Enter name of Wi-Fi connection: "
+	@echo %wifi% > %file%.txt
+	rem echo %file%.txt does not exist
+	pause
+	exit
+)
+set /p wfval=<%file%.txt
+rem echo %wfval%
+netsh wlan show networks | FIND "%wfval%" /I /C
 rem if %errorlevel% equ 1 (
 rem 	netsh interface set interface name="Wi-Fi" admin=enable
 rem ) else (
