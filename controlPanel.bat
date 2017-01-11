6f160b549aa9953f20e1aa572b73cc06b3584d82
kormin <abaotom14@gmail.com>
2017-01-11 11:21:44 +0800

    add datetime and uninstaller

--- controlPanel.bat
+++ controlPanel.bat
@@ -5,4 +5,11 @@ echo [1] Date and Time
 echo [2] Add/Remove Programs
 set /p val="Enter option: "
 echo %val%
-pause
+if %val% equ 1 (
+	control timedate.cpl
+) else if %val% equ 2 (
+	control appwiz.cpl
+
+) else (
+	echo Error. Option not available.
+)
