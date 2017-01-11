7fee55e763d70b6bc5685585b49773737dc979f4
kormin <abaotom14@gmail.com>
2017-01-11 11:30:11 +0800

    add more settings

--- controlPanel.bat
+++ controlPanel.bat
@@ -3,13 +3,20 @@ rem Control Panel settings
 echo Control Panel Options:
 echo [1] Date and Time
 echo [2] Add/Remove Programs
+echo [3] Network Connections
+echo [4] Power Management
+echo [5] Task Scheduler
 set /p val="Enter option: "
-echo %val%
 if %val% equ 1 (
 	control timedate.cpl
 ) else if %val% equ 2 (
 	control appwiz.cpl
-
+) else if %val% equ 3 (
+	control ncpa.cpl
+) else if %val% equ 4 (
+	control powercfg.cpl
+) else if %val% equ 5 (
+	control schedtasks
 ) else (
 	echo Error. Option not available.
 )
