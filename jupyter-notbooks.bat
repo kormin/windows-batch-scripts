e9f409fee4a5b7fb2a91bdc78319ee6b4fc4eb6c
Tom <abaotom14@gmail.com>
2020-10-10 09:42:00 +0800

    add jupyter notebooks command

---
+++ jupyter-notbooks.bat
@@ -0,0 +1,1 @@
+C:\Users\MyName\anaconda3\python.exe C:\Users\MyName\anaconda3\cwp.py C:\Users\MyName\anaconda3 C:\Users\MyName\anaconda3\python.exe C:\Users\MyName\anaconda3\Scripts\jupyter-notebook-script.py "C:\location"
