bf93c56b096b55f88d952fe6f27e071618babad2
Tom <abaotom14@gmail.com>
2020-10-31 08:40:42 +0800

    add clear screen

--- jupyter-notebooks.bat
+++ jupyter-notebooks.bat
@@ -1,1 +1,4 @@
+@echo off
+cls
+
 C:\Users\MyName\anaconda3\python.exe C:\Users\MyName\anaconda3\cwp.py C:\Users\MyName\anaconda3 C:\Users\MyName\anaconda3\python.exe C:\Users\MyName\anaconda3\Scripts\jupyter-notebook-script.py "C:\location" --no-browser
